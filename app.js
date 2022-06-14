e371898e1ae608bd54453ec202a89ed248356bcb
iashupandey <pashu068@gmail.com>
2022-06-14 16:15:27 +0530

    Ashu's 2nd commit

--- app.js
+++ app.js
@@ -15,23 +15,23 @@ function PageTransitions() {
     }
     
     allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            //resmove selected from the other btns
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
+      const id = e.target.dataset.id;
+      if(id){
+          //resmove selected from the other btns
+          sectBtns.forEach((btn) =>{
+              btn.classList.remove('active')
+          })
+          e.target.classList.add('active')
 
-            //hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
+          //hide other sections
+          sections.forEach((section)=>{
+              section.classList.remove('active')
+          })
 
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
+          const element = document.getElementById(id);
+          element.classList.add('active');
+      }
+  })
 }
   
 PageTransitions();
