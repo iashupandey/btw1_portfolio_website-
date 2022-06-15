76341874f5a0737acee3ad6beb52051c65b52bd3
ISHANK8800 <ishank8800kumar@gmail.com>
2022-06-16 02:30:50 +0530

    Header Content made by Ishank and DIkshit

--- app.js
+++ app.js
@@ -9,29 +9,31 @@ function PageTransitions() {
     sectBtn[i].addEventListener("click", function () {
       let currentBtn = document.querySelectorAll(".active-btn");
       currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",""  );
+        "active-btn",
+        ""
+      );
       this.className += " active-btn";
-    })
+    });
+  }
+
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //resmove selected from the other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
     }
-    
-    allSections.addEventListener('click', (e) =>{
-      const id = e.target.dataset.id;
-      if(id){
-          //resmove selected from the other btns
-          sectBtns.forEach((btn) =>{
-              btn.classList.remove('active')
-          })
-          e.target.classList.add('active')
-
-          //hide other sections
-          sections.forEach((section)=>{
-              section.classList.remove('active')
-          })
-
-          const element = document.getElementById(id);
-          element.classList.add('active');
-      }
-  })
+  });
 }
-  
+
 PageTransitions();
